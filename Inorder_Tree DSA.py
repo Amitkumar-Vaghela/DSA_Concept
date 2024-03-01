f551a80a34f8d4f46e43b30ac3ea287962d34e3d
Amit vaghela <138572130+Amitkumar-Vaghela@users.noreply.github.com>
2024-03-01 19:43:03 +0530

    Inorder_tree.py

---
+++ Inorder_Tree DSA.py
@@ -0,0 +1,38 @@
+class Node:
+ def __init__(self,v):  # def class for tree node
+   self.data=v
+   self.left=None
+   self.right=None
+  
+def printInorder(node):
+   if node is None:
+     return
+   printInorder(node.left) # left
+   print(node.data,end=' ') #node
+   printInorder(node.right)  #right
+   
+# def height(self,root):
+#   if root is None:
+#     return -1
+#   return max(self.height(root.left),self.height(root.right)) + 1
+   
+ #Drive unicode
+if __name__=='__main__':
+   root=Node(1)
+   root.left=Node(2)
+   root.right=Node(3)
+   root.left.left=Node(4)
+   root.left.right=Node(5)
+   root.right.right=Node(6)
+   root.right.left=Node(7)
+   print("Inorder Traping")
+   printInorder(root)
+   
+   
+
+
+
+
+ 
+  
+                                                                                       
