e1ca907796c772eec3ef533d118cd53443a035c6
Amit vaghela <138572130+Amitkumar-Vaghela@users.noreply.github.com>
2024-07-13 19:48:05 +0530

    Update binaraySearch.java

--- Sorting_Algo/binaraySearch.java
+++ Sorting_Algo/binaraySearch.java
@@ -16,4 +16,48 @@
     binarySearchSort(a,mid+1,j,x);
      }
 }
+
+here the exaple of binary seach 
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Enter the size of the array:");
+        int n = scanner.nextInt();
+
+        int[] a = new int[n];
+        System.out.println("Enter the elements of the array in sorted order:");
+        for (int i = 0; i < n; i++) {
+            a[i] = scanner.nextInt();
+        }
+
+        System.out.println("Enter the element to search:");
+        int x = scanner.nextInt();
+
+        int result = binarySearch(a, x);
+        if (result == -1) {
+            System.out.println("Element not found in the array");
+        } else {
+            System.out.println("Element found at index " + result);
+        }
+    }
+
+    public static int binarySearch(int[] a, int x) {
+        int i = 0;
+        int j = a.length - 1;
+        while (i <= j) {
+            int mid = (i + j) / 2;
+            if (a[mid] == x) {
+                return mid;
+            } else if (a[mid] < x) {
+                i = mid + 1;
+            } else {
+                j = mid - 1;
+            }
+        }
+        return -1;
+    }
+}
   
