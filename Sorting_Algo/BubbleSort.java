f422ee27e19b1ab8cb2320f595ec8fb57865cbe6
Amit vaghela <138572130+Amitkumar-Vaghela@users.noreply.github.com>
2024-07-16 15:59:37 +0530

    Update BubbleSort.java

--- Sorting_Algo/BubbleSort.java
+++ Sorting_Algo/BubbleSort.java
@@ -1,19 +1,23 @@
-class Solution {
-    public void BubbleSort(int[] nums){
-      int n = nums.length;
-      for(int i=0; i<n-1; i++){
-        for(int j=0; j<n-i-1; j++){
-          if(nums[j] < nums[j+1]){
-            int temp = nums[j];
-            nums[j] = nums[j+1];
-            nums[j+1] = temp;
-          }
+ static void bubbleSort(int arr[], int n)
+    {
+        int i, j, temp;
+        boolean swapped;
+        for (i = 0; i < n - 1; i++) {
+            swapped = false;
+            for (j = 0; j < n - i - 1; j++) {
+                if (arr[j] > arr[j + 1]) {
+                    
+                    // Swap arr[j] and arr[j+1]
+                    temp = arr[j];
+                    arr[j] = arr[j + 1];
+                    arr[j + 1] = temp;
+                    swapped = true;
+                }
+            }
+
+            // If no two elements were swapped by inner loop, then break
+            
+            if (swapped == false)
+                break;
         }
-      }
-      return nums;
     }
-}
-            
-            
-            
-            
