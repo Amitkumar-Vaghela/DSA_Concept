9c6d50b6e171100fa4a1dd92bba9e46b78e09a2c
Amit vaghela <138572130+Amitkumar-Vaghela@users.noreply.github.com>
2024-07-27 17:41:27 +0530

    Create rectangle.java

---
+++ Pattern/rectangle.java
@@ -0,0 +1,19 @@
+import java.util.*;
+class pattern{
+public static void main (string [] args){
+ for(int i =1; i<n; i++){
+  for(int j =1; j<n; j++){
+    system.out.print("*");
+  }
+  system.out.print();
+   }
+  }
+}
+
+// if your input is n = 5 then
+  
+   * * * * * 
+   * * * * *
+   * * * * *
+   * * * * *
+   * * * * *
