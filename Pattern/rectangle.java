2e4a0db4b6de261c413ccdd4368ebaf1df3becaf
Amit vaghela <138572130+Amitkumar-Vaghela@users.noreply.github.com>
2024-07-27 17:42:10 +0530

    Update rectangle.java

--- Pattern/rectangle.java
+++ Pattern/rectangle.java
@@ -1,19 +1,1 @@
-import java.util.*;
-class pattern{
-public static void main (string [] args){
- for(int i =1; i<n; i++){
-  for(int j =1; j<n; j++){
-    system.out.print("*");
-  }
-  system.out.print();
-   }
-  }
-}
 
-// if your input is n = 5 then
-  
-   * * * * * 
-   * * * * *
-   * * * * *
-   * * * * *
-   * * * * *
