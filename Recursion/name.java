5bb21d6a207d513293777dc56403917d20a185dd
Amit vaghela <138572130+Amitkumar-Vaghela@users.noreply.github.com>
2024-07-29 16:10:59 +0530

    Create name.java

---
+++ Recursion/name.java
@@ -0,0 +1,34 @@
+// print name using multiple function 
+
+public class Main{
+  public static void main (String[] args){
+    method();
+  }
+  
+ static void method()
+  {
+    System.out.println("Amit");
+    method2();
+  }
+  
+  static void method2()
+  {
+    System.out.println("Amit");
+    method3();
+  }
+  static void method3()
+  {
+    System.out.println("Amit");
+    method4();
+  }
+  static void method4()
+  {
+    System.out.println("Amit");
+    method5();
+  }
+  static void method5()
+  {
+    System.out.println("Amit");
+    
+  }
+}
