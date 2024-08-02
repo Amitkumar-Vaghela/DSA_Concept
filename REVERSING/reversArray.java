be25e42d301f57db648a5ba896274d612ee9704b
Amit vaghela <138572130+Amitkumar-Vaghela@users.noreply.github.com>
2024-08-02 17:06:43 +0530

    Update reversArray.java

--- REVERSING/reversArray.java
+++ REVERSING/reversArray.java
@@ -1,6 +1,7 @@
-// reversing an array by using three mathod such as follow down side 
-
+// reversing an array by using three mathod such as follow
 //  1) reversing using Extra auxilary arrray 
+//  2) Space-optimized iterative method
+//  3) recursive method
 
 public class Main {
    //Function to print array
@@ -24,3 +25,62 @@ public class Main {
       reverseArray(arr, n);
    }
 }
+
+// Solution 2: Space-optimized iterative method
+
+public class Main {
+   //Function to print array
+   static void printArray(int arr[], int n) {
+      System.out.print("Reversed array is:- \n");
+      for (int i = 0; i < n; i++) {
+         System.out.print(arr[i] + " ");
+      }
+   }
+   //Function to reverse array 
+   static void reverseArray(int arr[], int n) {
+      int p1 = 0, p2 = n - 1;
+      while (p1 < p2) {
+         int tmp = arr[p1];
+         arr[p1] = arr[p2];
+         arr[p2] = tmp;
+         p1++;
+         p2--;
+      }
+      printArray(arr, n);
+   }
+   public static void main(String[] args) {
+      int n = 5;
+      int arr[] = { 5, 4, 3, 2, 1};
+      reverseArray(arr, n);
+
+   }
+}  
+
+// solution 3: 
+
+public class Main {
+   //Function to print array
+   static void printArray(int arr[], int n) {
+      System.out.print("Reversed array is:- \n");
+      for (int i = 0; i < n; i++) {
+         System.out.print(arr[i] + " ");
+      }
+   }
+   //Function to reverse array using recursion
+   static void reverseArray(int arr[], int start, int end) {
+      if (start < end) {
+         int tmp = arr[start];
+         arr[start] = arr[end];
+         arr[end] = tmp;
+         reverseArray(arr, start + 1, end - 1);
+      }
+   }
+   public static void main(String[] args) {
+      int n = 5;
+      int arr[] = { 5, 4, 3, 2, 1};
+      reverseArray(arr, 0, n - 1);
+      printArray(arr, n);
+   }
+}
+
+
