44ed614de133b46a2494b348343a6e1b4a61e747
Amit vaghela <138572130+Amitkumar-Vaghela@users.noreply.github.com>
2024-08-06 21:40:03 +0530

    Update basic_ex1.java

--- Recursive_Tree/basic_ex1.java
+++ Recursive_Tree/basic_ex1.java
@@ -64,5 +64,13 @@ class RecursiveTree {
     tree.insert(8);
 
     tree.inOrderTraversal(); // prints: 2 3 4 5 6 7 8
+  }
 }
-}
+
+// explaination how to do recursive_tree
+
+    5
+   / \
+  3   7
+ / \ / \
+2  4 6  8
