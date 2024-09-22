747ec97f7182519e5e0d43cc43153f66e459c54c
Amit vaghela <138572130+Amitkumar-Vaghela@users.noreply.github.com>
2024-09-22 21:23:25 +0530

    Create BoatSavePeople_881.java

---
+++ leetcodePactice/BoatSavePeople_881.java
@@ -0,0 +1,19 @@
+import java.util.Arrays;
+public class Solution {
+    public int numRescueBoats(int[] people, int limit) {
+        Arrays.sort(people);  // Sort people by weight
+        int left = 0;  // Lightest person
+        int right = people.length - 1;  // Heaviest person
+        int boats = 0;  // Number of boats
+        
+        while (left <= right) {
+            if (people[left] + people[right] <= limit) {
+                left++;  // Pair the lightest with the heaviest
+            }
+            right--;  // Always take the heaviest person
+            boats++;  // One boat used
+        }
+        
+        return boats;  // Minimum number of boats required
+    }
+}
