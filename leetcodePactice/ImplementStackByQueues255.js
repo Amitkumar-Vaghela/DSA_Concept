a2af9f7b25a51389660dc00c6fed9b3ce2ac1d0f
Amit vaghela <138572130+Amitkumar-Vaghela@users.noreply.github.com>
2024-09-22 21:32:08 +0530

    Update and rename ImplementStackByQueues.js to ImplementStackByQueues255.js

--- leetcodePactice/ImplementStackByQueues255.js
+++ leetcodePactice/ImplementStackByQueues255.js
@@ -42,3 +42,20 @@ class MyStack {
         return main.isEmpty();
     }
 }
+
+/*
+Example 1:
+
+Input
+["MyStack", "push", "push", "top", "pop", "empty"]
+[[], [1], [2], [], [], []]
+Output
+[null, null, null, 2, 2, false]
+
+Explanation
+MyStack myStack = new MyStack();
+myStack.push(1);
+myStack.push(2);
+myStack.top(); // return 2
+myStack.pop(); // return 2
+myStack.empty(); // return False
